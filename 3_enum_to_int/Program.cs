7b98d0ef19a8955805b4b99103609c0bb5871a58
Phil <phil.wright@componentfactory.com>
2022-12-24 22:21:20 +1100

    C# Answer

---
+++ 3_enum_to_int/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Example
+{
+    public enum Suits
+    {
+        Spades,
+        Hearts,
+        Diamonds,
+        Clubs
+    }
+
+    class ExampleClass
+    {
+        static void Main(string[] args)
+        {
+            Suits x = Suits.Hearts;
+
+            int cast = (int)x;
+            Console.WriteLine($"ToInt: {x} = {cast}");
+
+            Suits y = (Suits)cast;
+            Console.WriteLine($"ToEnum: {y} = {cast}");
+
+            string parse = "Diamonds";
+            if (Enum.TryParse<Suits>(parse, out var z))
+            {
+                Console.WriteLine($"Parsed: {z} = {parse} = {(int)z}");
+            }
+        }
+    }
+}
