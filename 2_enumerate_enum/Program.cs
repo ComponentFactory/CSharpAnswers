5a13140441be828c1ee376c8b0c2880420a871f0
Phil <phil.wright@componentfactory.com>
2022-12-24 17:38:29 +1100

    #2 Answer

---
+++ 2_enumerate_enum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Example
+{
+    public enum Suits
+    {
+        Spades,
+        Hearts,
+        Diamonds,
+        Clubs
+    }
+
+    class ExampleClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nEnum.GetValues");
+            foreach(Suits i in Enum.GetValues<Suits>())
+            {
+                Console.WriteLine($"{i} = {(int)i}");
+            }
+
+            Console.WriteLine("\nEnum.GetNames");
+            foreach(string i in Enum.GetNames<Suits>())
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
--- COMMANDS.md
+++ COMMANDS.md
@@ -10,3 +10,6 @@ copied from github dotnet/runtime/.editorconfig
 # Add a new project, example
 dotnet new console -o ./1_string_vs_system
 dotnet sln add ./1_string_vs_system
+
+# CodeLens
+File -> Preferences -> Settings -> csharp.referencesCodeLens.enabled
