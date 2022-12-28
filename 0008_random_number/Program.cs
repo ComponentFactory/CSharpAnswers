2431c9fe0e6215c90a15ca7e2024351185c5b972
Phil <phil.wright@componentfactory.com>
2022-12-28 21:41:18 +1100

    Fix bugs.

--- 0008_random_number/Program.cs
+++ 0008_random_number/Program.cs
@@ -1,27 +1,27 @@
 ﻿Random rand = new Random();
 
 // >= 0 && < int.MaxValue
-int AnyPositive = rand.Next();
+int anyPositive = rand.Next();
 
 // >= 1024 && < int.MaxValue
-int AlmostAnyPostive = rand.Next(1024);
+int almostAnyPostive = rand.Next(1024);
 
 // >= 1 && < 7
-int DiceRoll = rand.Next(1, 7);
+int diceRoll = rand.Next(1, 7);
 
 // Upper limit is long.MaxValue
-long AnyPositive = rand.NextInt64();
-long AnyPositive = rand.NextInt64(1024);
-long AnyPositive = rand.NextInt64(1, 7);
+long anyPositive1 = rand.NextInt64();
+long anyPositive2 = rand.NextInt64(1024);
+long anyPositive3 = rand.NextInt64(1, 7);
 
 // Cannot specify range
-int s = rand.NextSingle();
-int d = rand.NextDouble();
+float s = rand.NextSingle();
+double d = rand.NextDouble();
 
 // Fill entire byte array
 byte[] bytes = new byte[8];
 rand.NextBytes(bytes);
 
 // Fill just span of a byte array
-var span = new Span<bytes>(bytes, 0, 4);
+var span = new Span<byte>(bytes, 0, 4);
 rand.NextBytes(span);
