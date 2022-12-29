06280da54122857ccf1f3ea75e7e435000647963
Phil <phil.wright@componentfactory.com>
2022-12-29 21:13:41 +1100

    Updated

--- 0009_null_coalescing/Program.cs
+++ 0009_null_coalescing/Program.cs
@@ -7,20 +7,15 @@ namespace _0009_null_coalescing
         static void Main(string[] args)
         {
             ExampleClass? example = null;
-
-            // Null coalescing assignment
-			example ??= new ExampleClass();      
+            example ??= new ExampleClass();   
 
             int num1 = example.Double(null);
             int num2 = example.Double(42);
             Console.WriteLine($"num1:{num1} num2:{num2}");
 
-            // This should work
             example.Name = "John Doe";
             Console.WriteLine($"Name:{example.Name}");
-
-            // This should throw an exception
-            example.Name = null!;
+            example.Name = null!;        
         }
 
         public int Double(int? number)
