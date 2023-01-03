37cd3dc45a1b3feb10fe183fdfb728a699a1a598
Phil <phil.wright@componentfactory.com>
2023-01-03 15:09:06 +1100

    Add Answer 14

---
+++ 0014_multiple_orderBy/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _0014_multiple_orderBy
+{
+    class ExampleClass
+    {
+        public class Person
+        {
+            public required int Age { get; set; }
+            public required string Name { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Person> list = new()
+            {
+                new Person() { Age = 30, Name = "Alice"},
+                new Person() { Age = 28, Name = "Bob"},
+                new Person() { Age = 28, Name = "Charlie"},
+                new Person() { Age = 28, Name = "David"},
+                new Person() { Age = 27, Name = "Ethan"},
+            };
+
+            // LINQ sort using 1 property
+            list = list.OrderBy(p => p.Age).ToList();
+
+            // LINQ sort using 2 levels of property
+            list = list.OrderBy(p => p.Age).ThenByDescending(p => p.Name).ToList();
+
+            Console.WriteLine("");
+            foreach(var persion in list)
+                Console.WriteLine($"{persion.Age} = {persion.Name}");
+        }
+    }
+}
--- COMMANDS.md
+++ COMMANDS.md
@@ -8,8 +8,8 @@ dotnet new gitignore
 copied from github dotnet/runtime/.editorconfig
 
 # Add a new project, example
-dotnet new console -o ./0013_multline_string_literal
-dotnet sln add ./0013_multline_string_literal
+dotnet new console -o ./0014_multiple_orderBy
+dotnet sln add ./0014_multiple_orderBy
 
 # CodeLens
 File -> Preferences -> Settings -> csharp.referencesCodeLens.enabled
