629fe00770f6cdd3a8059f1dc74343d9629b9590
Phil <phil.wright@componentfactory.com>
2023-01-14 18:11:40 +1100

    Add Answer 24

---
+++ 0025_chunk_link_results/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] names = new string[] { "Anne", "Bob", "Charlie", "Dick", "Enid" };
+
+var chunk = new List<string>();
+foreach(var name in names)
+{
+    chunk.Add(name);
+    if (chunk.Count == 2)
+    {
+        Console.WriteLine("Chunk");
+        foreach(var item in chunk)
+            Console.WriteLine($"  {item}");
+
+        chunk.Clear();
+    }
+}
+
+if (chunk.Count > 0) 
+{
+    Console.WriteLine("Chunk");
+    foreach(var name in chunk)
+        Console.WriteLine($"  {name}");
+}
+
+
+// foreach(var chunk in names.Chunk(2))
+// {
+//     Console.WriteLine("Chunk");
+//     foreach(var name in chunk)
+//         Console.WriteLine($"  {name}");
+// }
--- COMMANDS.md
+++ COMMANDS.md
@@ -8,9 +8,9 @@ dotnet new gitignore
 copied from github dotnet/runtime/.editorconfig
 
 # Add a new project, example
-dotnet new console -o ./0024_index_in_select
-dotnet sln add ./0024_index_in_select
-cd ./0024_index_in_select
+dotnet new console -o ./0025_chunk_link_results
+dotnet sln add ./0025_chunk_link_results
+cd ./0025_chunk_link_results
 
 # CodeLens
 File -> Preferences -> Settings -> csharp.referencesCodeLens.enabled
