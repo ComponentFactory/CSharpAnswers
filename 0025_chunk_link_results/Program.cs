2a5464d62c2f1f197265e8e6347bdf190dc85001
Phil <phil.wright@componentfactory.com>
2023-01-14 19:33:33 +1100

    Updated

--- 0025_chunk_link_results/Program.cs
+++ 0025_chunk_link_results/Program.cs
@@ -1,5 +1,6 @@
 ﻿string[] names = new string[] { "Anne", "Bob", "Charlie", "Dick", "Enid" };
 
+// Verbose pattern for processing chunks
 var chunk = new List<string>();
 foreach(var name in names)
 {
@@ -22,9 +23,10 @@ if (chunk.Count > 0)
 }
 
 
-// foreach(var chunk in names.Chunk(2))
-// {
-//     Console.WriteLine("Chunk");
-//     foreach(var name in chunk)
-//         Console.WriteLine($"  {name}");
-// }
+// Consice version using LINQ operator
+foreach(var chunk in names.Chunk(2))
+{
+    Console.WriteLine("Chunk");
+    foreach(var name in chunk)
+        Console.WriteLine($"  {name}");
+}
